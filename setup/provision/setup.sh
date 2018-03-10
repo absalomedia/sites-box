e2582fab8b946950e2f58654c46f95cdbc165fb6
Lawrence Meckan <media@absalom.biz>
2018-03-10 12:42:47 +1000

    More housekeeping

--- setup/provision/setup.sh
+++ setup/provision/setup.sh
@@ -174,8 +174,8 @@ sudo apt-get -y install php7.2-mysql
 reboot_webserver_helper
 
 echo "Updating MySQL."
-#sudo sed -ie 's/ 127.0.0.1/ 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
-#sudo wget -P /etc/mysql/mysql.conf.d/ https://gist.githubusercontent.com/Xeoncross/2d0503cee10a6374c627f0faaed9ea3f/raw/755f53a68770a31b4b56c14e11e944e9facb10b5/utf8mb4.cnf
+sudo sed -ie 's/ 127.0.0.1/ 0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo wget -P /etc/mysql/mysql.conf.d/ https://gist.githubusercontent.com/Xeoncross/2d0503cee10a6374c627f0faaed9ea3f/raw/755f53a68770a31b4b56c14e11e944e9facb10b5/utf8mb4.cnf
 sudo mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES; SET GLOBAL max_connect_errors=10000;"
 #sudo mysql_ssl_rsa_setup
 sudo service mysql restart
@@ -471,6 +471,7 @@ done
 
 # Disable default messages by removing execute privilege
 sudo chmod -x /etc/update-motd.d/*
+sudo updatedb
 
 # Set the new message
 echo "$WELCOME_MESSAGE" | sudo tee /etc/motd
