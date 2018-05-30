8d67f324f9737620fbfe88a775760548135900d3
Lawrence Meckan <media@absalom.biz>
2018-05-31 00:53:09 +1000

    Better file detection

--- setup/provision/mysql.sh
+++ setup/provision/mysql.sh
@@ -30,4 +30,12 @@ then
 sudo wget -P /etc/mysql/mysql.conf.d/ https://gist.githubusercontent.com/Xeoncross/2d0503cee10a6374c627f0faaed9ea3f/raw/755f53a68770a31b4b56c14e11e944e9facb10b5/utf8mb4.cnf
 fi
 sudo service mysql restart
+if [ ! -f "/var/www/vhosts/dbbackup.sh" ]
+then
+sudo wget -P /var/www/vhosts https://raw.githubusercontent.com/absalomedia/sites-box/master/setup/provision/dbbackup.sh
+fi
+if [ ! -f "/var/www/vhosts/dbrestore.sh" ]
+then
+sudo wget -P /var/www/vhosts https://raw.githubusercontent.com/absalomedia/sites-box/master/setup/provision/dbrestore.sh
+fi
 #sudo apt-mark hold mysql-server mysql-common mysql-server-5.7
